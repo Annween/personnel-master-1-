837cf7874e2959b79a80db5b8d4946380748b2d4
Annween <orianne.gruber@laposte.net>
2020-04-16 14:10:15 +0200

    Initial commit

---
+++ personnel-master/Personnel/src/commandLine/EmployeConsole.java
@@ -0,0 +1,62 @@
+package commandLine;
+
+import static commandLineMenus.rendering.examples.util.InOut.getString;
+
+
+import commandLineMenus.Menu;
+import commandLineMenus.Option;
+import personnel.Employe;
+
+public class EmployeConsole 
+{
+	private Option afficher(final Employe employe)
+	{
+		return new Option("Afficher l'employé", "l", () -> {System.out.println(employe);});
+	}
+
+	Menu editerEmploye(Employe employe)
+	{
+
+		Menu menu = new Menu("Gérer le compte " + employe.getNom(), "c");
+		menu.add(selectionnerEmploye());
+		menu.add(afficher(employe));
+		menu.add(changerNom(employe));
+		menu.add(changerPrenom(employe));
+		menu.add(changerMail(employe));
+		menu.add(changerPassword(employe));
+		menu.addBack("q");
+		return menu;
+	}
+
+	private List<Employe> selectionnerEmploye() {
+		return new List<Ligue>("Sélectionnez l'employe que vous voulez éditer", "s",
+				() -> new ArrayList<>(gestionPersonnel.getEmployee()),
+				(element) -> editerEmploye(element)
+		);
+	}
+
+
+	private Option changerNom(final Employe employe)
+	{
+		return new Option("Changer le nom", "n", 
+				() -> {employe.setNom(getString("Nouveau nom : "));}
+			);
+	}
+	
+	private Option changerPrenom(final Employe employe)
+	{
+		return new Option("Changer le prénom", "p", () -> {employe.setPrenom(getString("Nouveau prénom : "));});
+	}
+	
+	private Option changerMail(final Employe employe)
+	{
+		return new Option("Changer le mail", "e", () -> {employe.setMail(getString("Nouveau mail : "));});
+	}
+	
+	private Option changerPassword(final Employe employe)
+	{
+		return new Option("Changer le password", "x", () -> {employe.setPassword(getString("Nouveau password : "));});
+	}
+	
+
+}
